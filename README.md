c359bc44422c5dbcc256edacfa711d9be2d0d671
leolm83 <leonardo.mateus01@hotmail.com>
2024-02-12 00:34:30 -0300

    readme e adicao swagger

---
+++ README.md
@@ -0,0 +1,22 @@
+# Places Service Api
+
+o projeto é uma api de localizacoes do desafio:
+[clickbus](https://github.com/RocketBus/quero-ser-clickbus/tree/master/testes/backend-developer)
+
+Ferramentas utilizadas:
+
+* Spring boot 3
+* Java 17
+* MapStruct
+* Jackson DataBinding
+* Lombok
+* Slugify
+
+
+*possui algumas mudanças/melhorias principalmente no uso do MapStruct,Lombok e Swagger e tambem na forma dos testes.*
+
+este projeto foi incrementado e baseado em:
+[github](https://github.com/giuliana-bezerra/place-service)
+
+Thanks Giuliana Bezerra por proporcionar este conteudo de forma gratuita em:
+[youtube](https://www.youtube.com/watch?v=SsWZ4O9iWuo)
--- src/main/java/com/leolm/placeservice/controllers/PlaceController.java
+++ src/main/java/com/leolm/placeservice/controllers/PlaceController.java
@@ -4,6 +4,11 @@ import com.github.slugify.Slugify;
 import com.leolm.placeservice.domain.services.PlaceService;
 import com.leolm.placeservice.dtos.PlaceRequestDto;
 import com.leolm.placeservice.dtos.PlaceResponseDto;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +28,15 @@ public class PlaceController {
     }
 
     @PostMapping
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = PlaceResponseDto.class))
+                            })
+            })//todo adicionar response code aqui
     public ResponseEntity<PlaceResponseDto> create(@Valid @RequestBody PlaceRequestDto placeRequestDto){
         placeRequestDto.setSlug(slugify.slugify(placeRequestDto.getName()));
         PlaceResponseDto createdPlace = placeService.create(placeRequestDto);
